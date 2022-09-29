97cf8d456c3ab286eabedaa2b789af86f64cecee
shayanfpg9 <shfqaen@gmail.com>
2022-09-29 13:25:15 +0330

    make loader

---
+++ scripts/loader.js
@@ -0,0 +1,22 @@
+document.head.innerHTML += `<link id="loader-style" rel="stylesheet" href="styles/loader.css">`;
+
+document.querySelector("html").style.overflowY = "hidden";
+
+const loadBox = document.createElement("section");
+loadBox.id = "load-box";
+document.body.appendChild(loadBox);
+
+const loader = document.createElement("div");
+loader.classList.add("loader");
+loadBox.appendChild(loader);
+
+function onPageLoad() {
+  setTimeout(() => {
+    loader.classList.add("fade");
+    
+    setTimeout(() => {
+      loadBox.remove();
+      $("#loader-style").remove();
+    }, 500);
+  }, 2000);
+}
