e171051ea04206fe5da6d078f987ab4d2aa9aca0
shayanfpg9 <shfqaen@gmail.com>
2022-09-29 13:56:11 +0330

    add other pages into onDatasLoaded function rules

--- scripts/global.js
+++ scripts/global.js
@@ -27,81 +27,81 @@ checkUrl([
   };
 
   if (!$("#header")) loaded.header = true;
+  else
+    $("#header").addEventListener("load", function () {
+      const header = $("#header").contentDocument;
 
-  $("#header").addEventListener("load", function () {
-    const header = $("#header").contentDocument;
-
-    header.querySelectorAll(".nav-link").forEach((element, i) => {
-      if (i == 5) {
-        element.setAttribute(
-          "event",
-          location.href + element.getAttribute("event")
-        );
-
-        if (dark) {
-          element.innerHTML = element.innerHTML
-            .replace("moon", "sun")
-            .replace("شب", "روز");
-
+      header.querySelectorAll(".nav-link").forEach((element, i) => {
+        if (i == 5) {
           element.setAttribute(
             "event",
-            element.getAttribute("event").replace(/darkmode/g, "lightmode")
+            location.href + element.getAttribute("event")
           );
-        }
-      }
 
-      element.onclick = () => {
-        const attr = element.getAttribute("event");
-        if (attr === "showLicense") {
-          showLicense();
-        } else {
-          goto(attr);
+          if (dark) {
+            element.innerHTML = element.innerHTML
+              .replace("moon", "sun")
+              .replace("شب", "روز");
+
+            element.setAttribute(
+              "event",
+              element.getAttribute("event").replace(/darkmode/g, "lightmode")
+            );
+          }
         }
+
+        element.onclick = () => {
+          const attr = element.getAttribute("event");
+          if (attr === "showLicense") {
+            showLicense();
+          } else {
+            goto(attr);
+          }
+        };
+      });
+
+      header.querySelector("a.special").onclick = () => {
+        window.scrollTo(0, innerHeight);
       };
+
+      header.querySelector("a.navbar-brand").onclick = () => {
+        goto("index.html");
+      };
+
+      header.querySelector(".navbar-toggler-icon").onclick = () => {
+        $("#header").setAttribute("open", "");
+        $("html").style.overflowY = "hidden";
+      };
+
+      header.querySelector(".btn-close").onclick = () => {
+        $("#header").removeAttribute("open");
+        $("html").style.overflowY = "";
+      };
+
+      loaded.header = true;
+
+      setTimeout(() => {
+        onDatasLoaded(loaded.header, loaded.footer);
+      }, 1500);
     });
-
-    header.querySelector("a.special").onclick = () => {
-      window.scrollTo(0, innerHeight);
-    };
-
-    header.querySelector("a.navbar-brand").onclick = () => {
-      goto("index.html");
-    };
-
-    header.querySelector(".navbar-toggler-icon").onclick = () => {
-      $("#header").setAttribute("open", "");
-      $("html").style.overflowY = "hidden";
-    };
-
-    header.querySelector(".btn-close").onclick = () => {
-      $("#header").removeAttribute("open");
-      $("html").style.overflowY = "";
-    };
-
-    loaded.header = true;
-
-    setTimeout(() => {
-      onDatasLoaded(loaded.header, loaded.footer);
-    }, 1500);
-  });
 
   if (!$("#footer")) loaded.footer = true;
+  else
+    $("#footer").onload = () => {
+      const footer = $("#footer").contentDocument;
+      footer.querySelectorAll(".nav-link").forEach((element) => {
+        element.onclick = () => {
+          const attr = element.getAttribute("event");
+          goto(attr, "_blank");
+        };
+      });
 
-  $("#footer").onload = () => {
-    const footer = $("#footer").contentDocument;
-    footer.querySelectorAll(".nav-link").forEach((element) => {
-      element.onclick = () => {
-        const attr = element.getAttribute("event");
-        goto(attr, "_blank");
-      };
-    });
+      loaded.footer = true;
 
-    loaded.footer = true;
-
-    setTimeout(() => {
-      onDatasLoaded(loaded.header, loaded.footer);
-    }, 1500);
-  };
+      setTimeout(() => {
+        onDatasLoaded(loaded.header, loaded.footer);
+      }, 1500);
+    };
 
   setTimeout(() => {
     onDatasLoaded(loaded.header, loaded.footer);
@@ -153,9 +153,12 @@ function onDatasLoaded(header, footer) {
             $("main section:not(.document) ol").innerHTML == "") &&
           $("#error")) == undefined,
       doc = $(".document") == undefined && $("#error") == undefined,
-      versions = $("main .col-12") == undefined;
+      versions = $("main .col-12") == undefined,
+      examples = ($(".tab-content") && $(".nav.nav-tabs")) == undefined,
+      about = !location.href.includes("about"),
+      index = !location.href.includes("index") && location.pathname !== "/";
 
-    if (menu && doc && versions) {
+    if (menu && doc && versions && examples && about && index) {
       console.log("redirect");
       location.reload();
     } else if (!$("#theme")) {
