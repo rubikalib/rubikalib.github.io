a6da73c5b2942becb15466abd0e46919515e9e44
Bahman <bahmanahmadi.mail@gmail.com>
2022-12-02 16:52:39 +0330

    back to old design because new version has some main bugs

--- scripts/examples.js
+++ scripts/examples.js
@@ -1,104 +1,92 @@
-$("#header").onload = () => {
-  fetch(`sources.json?${Date.now()}`)
-    .then((data) => data.json())
-    .then((result) => {
-      const header = $("#header").contentDocument,
-        h2 = header.querySelector(".firstViewChild > h2"),
-        special = header.querySelector(".firstViewChild > .special");
+function XHR() {
+	var db;
+	let xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function () {
+		if (this.status == 200) {
+			db = JSON.parse(this.responseText);
+			for (var groupSource of db.groups) {
+				document.getElementById("groups").innerHTML += `
+				<div class="item">
+				<a href="${groupSource.URL}"><div class="right">${groupSource.name}</div></a>
+				<div class="left">
+				${groupSource.developer}
+				</div>
+				</div>
+				`;
+			}
+			for (var channelSource of db.channels) {
+				document.getElementById("channels").innerHTML += `
+				<div class="item" onclick="goto('${channelSource.URL}');">
+				<a href="${channelSource.URL}"><div class="right">${channelSource.name}</div></a>
+				<div class="left">
+				${channelSource.developer}
+				</div>
+				</div>
+				`;
+			}
+			for (var pvSource of db.privates) {
+				document.getElementById("privates").innerHTML += `
+				<div class="item" onclick="goto('${pvSource.URL}');">
+				<a href="${pvSource.URL}"><div class="right">${pvSource.name}</div></a>
+				<div class="left">
+				${pvSource.developer}
+				</div>
+				</div>
+				`;
+			}
+			for (var multiSource of db.multiplatforms) {
+				document.getElementById("multiplatforms").innerHTML += `
+				<div class="item" onclick="goto('${multiSource.URL}');">
+				<a href="${multiSource.URL}"><div class="right">${multiSource.name}</div></a>
+				<div class="left">
+				${multiSource.developer}
+				</div>
+				</div>
+				`;
+			}
+		}
+	};
+	var d = new Date();
+	xhr.open("GET", `../sources.json?${d.getTime()}`, false);
+	xhr.send();
+}
+setInterval(XHR(), 1000);
 
-      h2.innerHTML = "ربات های شما 🐱‍👤";
-      special.innerHTML = "بریم ببینیم چی ساختین 🤯";
+// theme settings
+if (window.localStorage.getItem("theme") === "dark") {
+	document.getElementsByTagName("head")[0].innerHTML +=
+	'<link rel="stylesheet" href="styles/dark/index.css"/><link rel="stylesheet" href="styles/dark/examples.css"/>';
+	document.getElementsByTagName("nav")[0].className =
+	"nav bg-dark toolbar fixed-top";
+}
 
-      const topics = Object.keys(result);
+document.querySelectorAll("div.item").forEach((el) => {
+	el.onclick = async function (e) {
+		let ripple = document.createElement("span");
+		ripple.classList.add("ripple");
 
-      let firstTopic = 0;
+		this.appendChild(ripple);
 
-      for (const topic of topics) {
-        const main = $("main > nav");
-        main.innerHTML += `
-        <button
-          class="nav-link"
-          data-bs-target="#${topic}-content"
-          data-bs-toggle="tab"
-          id="${topic}-btn"
-        >
-          ${result[topic].name}
-        </button>
-      `;
+		let x = e.clientX - e.target.offsetLeft;
+		let y = e.clientY - e.target.offsetTop;
 
-        if (result[topic].items.length > 0) {
-          result[topic].items.reverse();
-          result[topic].items.forEach((item) => {
-            const developerLink = new URL(
-              item.URL.replace(/(http|https):\/\/github.com(\/*)/i, "")
-                .split("/")
-                .splice(0, 2)
-                .join("/"),
-              "https://github.com"
-            ).href;
+		ripple.style.background = "#ccc";
+		ripple.style.left = `${x}px`;
+		ripple.style.top = `${y}px`;
 
-            $(`main ul`).innerHTML += `
-            <li class="tab-pane nav-item fade" id="${topic}-content">
-              <a href="javascript:void(0)" class="nav-link product-name" download="${item.URL}">${item.name}</a>
-              <a class="developer" href="${developerLink}" target="_blank">${item.developer}<a>
-            </li>
-          `;
-          });
-        } else {
-          if (topic == topics[firstTopic] && firstTopic + 1 != topics.length) {
-            firstTopic++;
-          }
+		setTimeout(() => {
+			ripple.remove();
+		}, 800);
+		await new Promise((resolve) => setTimeout(resolve, 600));
+	};
+});
 
-          $(`main ul`).innerHTML += `
-        <li class="tab-pane nav-item fade d-flex justify-content-center" id="${topic}-content">
-          <i class="fa fa-cloud"></i>
-          <span class="empty">این بخش خالی است</span>
-        </li>
-      `;
-        }
-      }
-
-      $(".product-name", (result) => {
-        return result.length ? result : [result];
-      }).forEach((el) => {
-        const download = el.getAttribute("download"),
-          name = download.split("/").pop();
-
-        el.addEventListener("click", () => {
-          Swal.fire({
-            title: "ایا واقعا میخواهید این فایل را دانلود کنید ؟ ",
-            icon: "question",
-            text: `نام فایل: ${name}`,
-            showDenyButton: true,
-            showCloseButton: true,
-            confirmButtonText: "بله",
-            denyButtonText: "خیر",
-          }).then((result) => {
-            if (result.isConfirmed) {
-              goto(download, "_blank");
-              sendMessage("دانلود با موفقیت انجام شد", "success");
-            } else if (result.isDenied) {
-              sendMessage("عملیات با موفقیت لغو شد", "warning");
-            }
-          });
-        });
-      });
-
-      firstTopic = topics[firstTopic];
-      $(`#${firstTopic}-btn`).classList.add("active");
-      $(`#${firstTopic}-content`).classList.add("active", "show");
-
-      checkUrl(
-        [
-          {
-            key: null,
-            do: (topic) => {
-              scrollTo(0, innerHeight);
-              $(`#${topic}-btn`).click();
-            },
-          },
-        ],
-        ["#"]
-      );
-    });
-};
+function goto(link, target = "_self") {
+	try {
+		document.body.removeChild(document.getElementById("clickme"));
+	} catch (e) {
+		console.log(e);
+	}
+	document.body.innerHTML += `<a href="${link}" target="${target}" id="clickme"></a>`;
+	document.getElementById("clickme").click();
+}
---
+++ scripts/index.js
@@ -0,0 +1,132 @@
+document.getElementById("firstView").style = `height: ${window.screen.height}px;`;
+
+var Click = 0;
+var menuButton = document.getElementsByClassName("btn-menu")[0];
+var lastSource = "";
+var texts = [
+	"<i class='fa fa-book'></i> مستندات",
+	"<i class='fa fa-bullhorn'></i> اخبار",
+	"<i class='fa fa-comments'></i> گفتگو",
+	"<i class='fa fa-user-circle-o'></i> پشتیبانی",
+	"<i class='fa fa-moon-o'></i> حالت شب",
+	"<i class='fa fa-code'></i> نمونه‌ها",
+	"<i class='fa fa-file'></i> مجوز",
+	"<i class='fa fa-users'></i> درباره‌ما",
+];
+var events = [
+	"goto(\"docs.html\");",
+	"goto(\"https://t.me/rubikalib\");",
+	"goto(\"https://t.me/rubikalibGP\");",
+	"goto(\"https://t.me/Bprogrammer\");",
+	"goto(\"?darkmode\");",
+	"goto(\"examples.html\");",
+	"showLicense();",
+	"goto(\"about.html\");"
+];
+async function menu() {
+	if (Click%2 == 0) {
+		// open menu
+		lastSource = document.getElementsByTagName("div")[0].innerHTML;
+		document.body.style = "padding: 0;margin: 0;";
+		document.body.className = "items";
+		document.getElementsByTagName("div")[0].innerHTML = `<nav class="nav toolbar fixed-top"><img src="assets/logo.png" alt="[logo]" class="toolbar-image"/><span class="toolbar-text">rubika</span></nav><div class='itemss'><br/><br/><center class='items-org'></center></div><br/><br/>`;
+		for (var i = 1; i <= texts.length; i++) {
+			document.getElementsByClassName("items-org")[0].innerHTML += `<button class='btn btn-item btn-outline-dark col-4' onclick='${events[i-1]}'>${texts[i-1]}</button>`;
+		}
+		menuButton.innerHTML = `<i class="fa fa-close"></i>`;
+	} else {
+		// close menu
+		menuButton.innerHTML = `<i class="fa fa-bars"></i>`;
+		document.getElementsByTagName('div')[0].innerHTML = lastSource;
+		document.body.className = "";
+	}
+	Click++;
+}
+
+var lastLocation = 0;
+function scrolled(count) {
+	if (count.scrollY < lastLocation) {
+		menuButton.hidden = false;
+	} else {
+		menuButton.hidden = true;
+		menuButton.classList.add('hided');
+	}
+	lastLocation = count.scrollY;
+}
+
+function checkUrl() {
+
+	var data = document.URL.split("?")[1];
+	if (data === "darkmode") {
+		localStorage.setItem("theme", "dark");
+	} else if (data === "lightmode") {
+		localStorage.setItem("theme", "light");
+	}
+
+	if (window.localStorage.getItem("theme") === "dark") {
+		document.head.innerHTML += '<link rel="stylesheet" href="styles/dark/index.css"/>';
+		//document.body.innerHTML += '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@sweetalert2/themes@5.0.2/wordpress-admin/wordpress-admin.css" />';
+		texts[4] = "<i class='fa fa-sun-o'></i> حالت روز";
+		events[4] = "goto(\"?lightmode\");";
+		document.getElementsByClassName("btn-menu")[0].onclick = async function(e) {
+			if (Click%2 == 0) {
+				// open menu
+				lastSource = document.getElementsByTagName("div")[0].innerHTML;
+				document.body.style = "padding: 0;margin: 0;";
+				document.body.className = "items";
+				document.getElementsByTagName("div")[0].innerHTML = `<nav class="nav toolbar fixed-top"><img src="assets/logo.png" alt="[logo]" class="toolbar-image"/><span class="toolbar-text">rubika</span></nav><div class='itemss'><br/><br/><center class='items-org'></center></div><br/><br/>`;
+				for (var i = 1; i <= texts.length; i++) {
+					document.getElementsByClassName("items-org")[0].innerHTML += `<button class='btn btn-item btn-outline-light col-4' onclick='${events[i-1]}'>${texts[i-1]}</button>`;
+				}
+				menuButton.innerHTML = `<i class="fa fa-close"></i>`;
+			} else {
+				// close menu
+				menuButton.innerHTML = `<i class="fa fa-bars"></i>`;
+				document.getElementsByTagName('div')[0].innerHTML = lastSource;
+				document.body.className = "";
+			}
+			Click++;
+		};
+	}
+}
+
+async function goto(link) {
+	// wait for loading hover effects
+	await new Promise(resolve => setTimeout(resolve, 260));
+
+	// loading url
+	document.getElementById("clickMe").href = link;
+	document.getElementById('clickMe').click();
+}
+
+function showLicense() {
+	localStorage.getItem("theme") == "dark" ? document.body.innerHTML += '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@sweetalert2/themes@5.0.2/wordpress-admin/wordpress-admin.css" />"/': "";
+	Swal.fire(
+		"MIT License",
+		`MIT License
+
+		Copyright (c) Bahman Ahmadi\n\n
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy\n
+		of this software and associated documentation files (the "Software"), to dea\n
+		in the Software without restriction, including without limitation the rights\n
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n
+		copies of the Software, and to permit persons to whom the Software is\n
+		furnished to do so, subject to the following conditions:\n\n
+
+		The above copyright notice and this permission notice shall be included in a\n
+		copies or substantial portions of the Software.\n\n
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM\n
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN TH\n
+		SOFTWARE.`,
+		"info",
+		100
+	).then(function() {
+			document.styleSheets.pop();
+		});
+}
